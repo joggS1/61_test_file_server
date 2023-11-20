576fc5c92606d3ff475d91eb4df772d1b370c4d2
Nikita <n.lukin@storageup.net>
2023-11-21 00:45:56 +0300

    possible deployment fix

--- src/api/http/file/getUserFile.ts
+++ src/api/http/file/getUserFile.ts
@@ -2,12 +2,13 @@ import ApiResponseHandler from '../apiResponseHandler';
 import { Request, Response, NextFunction } from 'express';
 import { EchoMessage } from '../../../ts/types';
 import FileService from '../../../services/fileService';
+import fs from 'fs'
 
 export default async (req: Request<{}, {}, {}, {fileName: string }>, res: Response<EchoMessage>, next: NextFunction) => {
     try {
         const file = FileService.getFile(req.query.fileName);
-        // await ApiResponseHandler.success(req, res, file);
-        res.sendFile(file!);
+        await ApiResponseHandler.success(req, res, fs.readFileSync(file!));
+        // res.sendFile(file!);
     } catch (error) {
         await ApiResponseHandler.error(req, res, error);
     }

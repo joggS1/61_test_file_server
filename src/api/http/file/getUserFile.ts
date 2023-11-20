c007e8b57f84e171fe81e9a6008bb7587dd74250
Nikita <n.lukin@storageup.net>
2023-11-21 00:05:15 +0300

    fix + deploy

--- src/api/http/file/getUserFile.ts
+++ src/api/http/file/getUserFile.ts
@@ -3,9 +3,9 @@ import { Request, Response, NextFunction } from 'express';
 import { EchoMessage } from '../../../ts/types';
 import FileService from '../../../services/fileService';
 
-export default async (req: Request<{}, {}, { fileName: string }>, res: Response<EchoMessage>, next: NextFunction) => {
+export default async (req: Request<{}, {}, {}, {fileName: string }>, res: Response<EchoMessage>, next: NextFunction) => {
     try {
-        const file = FileService.getFile(req.body.fileName);
+        const file = FileService.getFile(req.query.fileName);
         // await ApiResponseHandler.success(req, res, file);
         res.sendFile(file!);
     } catch (error) {

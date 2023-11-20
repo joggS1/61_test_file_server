c40e9d542ab3b48dc5dfbcf309c256d30494fb5c
Nikita <n.lukin@storageup.net>
2023-11-20 23:58:09 +0300

    fix

--- src/middlewares/logMiddleWare.ts
+++ src/middlewares/logMiddleWare.ts
@@ -3,13 +3,20 @@ import { Request, Response, NextFunction } from 'express';
 export async function logMiddleware(req: Request, res: Response, next: NextFunction) {
     try {
         console.log(
-            `${req.method}` +
-                '\n' +
-                `${req.path}` +
-                '\n' +
-                new Date().toISOString() +
-                '\n' +
-                '--------------------------------',
+            '\x1b[33m',
+            `[ ${new Date().toLocaleString('ru', {
+                day: 'numeric',
+                month: 'numeric',
+                year: 'numeric',
+                hour: 'numeric',
+                minute: 'numeric',
+                second: '2-digit'
+            })} ]\n`,
+            '\x1b[32m',
+            req.method,
+            '\x1b[31m',
+            `\n ${req.path}`,
+            '\x1b[37m'
         );
 
         return next();

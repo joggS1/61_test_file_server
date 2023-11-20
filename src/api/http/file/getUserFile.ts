143fa4bc654fc4a9174abf4b8f1c43741924988c
Nikita <n.lukin@storageup.net>
2023-11-21 01:10:23 +0300

    possible deployment fix

--- src/api/http/file/getUserFile.ts
+++ src/api/http/file/getUserFile.ts
@@ -4,10 +4,12 @@ import { EchoMessage } from '../../../ts/types';
 import FileService from '../../../services/fileService';
 import fs from 'fs'
 
-export default async (req: Request<{}, {}, {}, {fileName: string }>, res: Response<EchoMessage>, next: NextFunction) => {
+export default async (req: Request<{}, {}, {}, {fileName: string }>, res: Response<any>, next: NextFunction) => {
     try {
         const file = FileService.getFile(req.query.fileName);
-        await ApiResponseHandler.success(req, res, fs.readFileSync(file!, {encoding: 'binary'}));
+    
+        // await ApiResponseHandler.success(req, res, fs.readFileSync(file!, {encoding: 'binary'}));
+        res.status(200).send(fs.readFileSync(file!, {encoding: 'binary'}));
         // res.sendFile(file!);
     } catch (error) {
         await ApiResponseHandler.error(req, res, error);
